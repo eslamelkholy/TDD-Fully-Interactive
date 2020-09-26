7103806146dab32214a4b9bccf07b34f9c412749
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-09-26 15:40:00 +0200

    Refactor Code and Make It Green Still have more Works With Centrailize Error Handling Soon

--- src/server.js
+++ src/server.js
@@ -5,8 +5,12 @@ const app = express();
 
 app.get("/users/:name", async (req, res) => {
   const { name } = req.params;
-  const user = await db.getUserByName(name);
-  res.json(user);
+  try {
+    const user = await db.getUserByName(name);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 export { app };
