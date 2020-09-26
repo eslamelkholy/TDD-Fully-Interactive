c42005aeee780765499b5fd2500acd886d65bcf8
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-09-26 15:33:36 +0200

    Assertion Status Code, Content-Type and The Response Type

--- src/server.test.js
+++ src/server.test.js
@@ -9,10 +9,15 @@ describe("GET /users/:name", () => {
   it("Sends The Correct Response When User With This Username is Exists", async () => {
     const fakeData = DummyData[0];
     const stub = sinon.stub(db, "getUserByName").resolves(fakeData);
-    await request(app).get("/users/abc").expect(200).expect("Content-Type", /json/).expect(fakeData);
-
+    await request(app)
+      .get("/users/abc")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        expect(fakeData).excludingEvery("_id").to.deep.equal(res.body);
+      });
     expect(stub.getCall(0).args[0]).to.equal("abc");
-
     stub.restore();
   });
+  it("Sends The Correct Response When There is an Error", async () => {});
 });
